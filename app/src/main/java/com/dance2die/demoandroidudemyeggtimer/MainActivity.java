5c027422b89e86d181efcceefa361ce444d8f642
dance2die <dontbothermewithspam@gmail.com>
2016-03-26 07:52:07 -0400

    Egg timer implementation completed.

--- app/src/main/java/com/dance2die/demoandroidudemyeggtimer/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemyeggtimer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.dance2die.demoandroidudemyeggtimer;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
@@ -10,6 +11,7 @@ import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -17,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
 
     private SeekBar timerSeekBar;
     private TextView timerTextView;
+    private boolean isCounterActive = false;
+    private Button controllerButton;
+    private CountDownTimer countDownTimer;
 
 
     private void updateTimer(int secondsLeft) {
@@ -27,22 +32,44 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void controlTimer(View view){
-        final int ONE_SECOND = 1000;
-        // 100 is a hack. Timer starts right away, we need to add 100 at least to make the timer start not right away.
-        long timerSeconds = timerSeekBar.getProgress() * ONE_SECOND + 100;
+        if (isCounterActive) {
+            resetTimer(countDownTimer);
+        } else {
+            isCounterActive = true;
+            timerSeekBar.setEnabled(!isCounterActive);
+            controllerButton.setText("Stop");
 
-        new CountDownTimer(timerSeconds, ONE_SECOND){
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updateTimer((int) (millisUntilFinished / ONE_SECOND));
-            }
+            final int ONE_SECOND = 1000;
+            // 100 is a hack. Timer starts right away, we need to add 100 at least to make the timer start not right away.
+            long timerSeconds = timerSeekBar.getProgress() * ONE_SECOND + 100;
 
-            @Override
-            public void onFinish() {
-                timerTextView.setText("0:00");
-                Log.i("finished", "timer done!");
-            }
-        }.start();
+            countDownTimer = new CountDownTimer(timerSeconds, ONE_SECOND){
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer((int) (millisUntilFinished / ONE_SECOND));
+                }
+
+                @Override
+                public void onFinish() {
+                    resetTimer(this);
+                    Log.i("finished", "timer done!");
+                    isCounterActive = false;
+
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.airhorn);
+                    mediaPlayer.start();
+                }
+            }.start();
+        }
+    }
+
+    private void resetTimer(CountDownTimer countDownTimer) {
+        timerTextView.setText("0:30");
+        timerSeekBar.setProgress(30);
+        assert countDownTimer != null;
+        countDownTimer.cancel();
+        controllerButton.setText("Go!");
+        timerSeekBar.setEnabled(true);
+        isCounterActive = false;
     }
 
     @Override
@@ -64,6 +91,7 @@ public class MainActivity extends AppCompatActivity {
 
         timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
         timerTextView = (TextView) findViewById(R.id.timerTextView);
+        controllerButton = (Button) findViewById(R.id.controllerButton);
 
         timerSeekBar.setMax(600);   // 10 minutes = 60 seconds * 10
         timerSeekBar.setProgress(30);   // 30 seconds
