d9a50c1c67a4091798570b207bca0186e4f8af19
dance2die <dontbothermewithspam@gmail.com>
2016-03-26 07:34:01 -0400

    timer works correctly

--- app/src/main/java/com/dance2die/demoandroidudemyeggtimer/MainActivity.java
+++ app/src/main/java/com/dance2die/demoandroidudemyeggtimer/MainActivity.java
@@ -1,10 +1,12 @@
 package com.dance2die.demoandroidudemyeggtimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +14,36 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SeekBar timerSeekBar;
+    private TextView timerTextView;
+
+
+    private void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - (minutes * 60);
+
+        timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+    }
+
+    public void controlTimer(View view){
+        final int ONE_SECOND = 1000;
+        // 100 is a hack. Timer starts right away, we need to add 100 at least to make the timer start not right away.
+        long timerSeconds = timerSeekBar.getProgress() * ONE_SECOND + 100;
+
+        new CountDownTimer(timerSeconds, ONE_SECOND){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer((int) (millisUntilFinished / ONE_SECOND));
+            }
+
+            @Override
+            public void onFinish() {
+                timerTextView.setText("0:00");
+                Log.i("finished", "timer done!");
+            }
+        }.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,18 +62,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        SeekBar timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
-        final TextView timerTextView = (TextView) findViewById(R.id.timerTextView);
+        timerSeekBar = (SeekBar) findViewById(R.id.timerSeekBar);
+        timerTextView = (TextView) findViewById(R.id.timerTextView);
 
         timerSeekBar.setMax(600);   // 10 minutes = 60 seconds * 10
         timerSeekBar.setProgress(30);   // 30 seconds
         timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = progress / 60;
-                int seconds = progress - (minutes * 60);
-
-                timerTextView.setText(String.format("%d:%02d", minutes, seconds));
+                updateTimer(progress);
             }
 
             @Override
